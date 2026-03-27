388a21e99059dd8f3ad527d0413f1c66c01d83d6
Willie Northway <willn@umich.edu>
2026-03-26 22:49:23 -0400

    Drop the Tango icon library.

--- public/logic/agreement.php
+++ public/logic/agreement.php
@@ -2,8 +2,6 @@
 	if ( $PUBLIC_USER ) {
 		echo <<<EOHTML
 			<div class="return_link">
-				<img src="display/images/tango/32x32/actions/go-previous.png"
-					class="tango" alt="previous">
 				<a href="?id=agreement">Back to listing</a>
 			</div>
 EOHTML;
