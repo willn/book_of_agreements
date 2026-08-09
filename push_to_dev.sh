9975a88a7ae924dea851c4cf5b233f63ec66ff8a
Willie Northway <willn@umich.edu>
2026-08-09 00:09:16 -0400

    Remove reference to ssh key

--- push_to_dev.sh
+++ push_to_dev.sh
@@ -4,6 +4,6 @@
 rsync -e 'ssh' -avz --exclude '*.swp' public/ gocoho@gocoho.org:/home/gocoho/public_html/boa_dev/
 
 #reset permissions
-ssh -i ~/.ssh/id_dsa gocoho@gocoho.org 'cd ~/public_html/boa_dev/ && ~/bin/fix_web_perms.sh';
+ssh gocoho@gocoho.org 'cd ~/public_html/boa_dev/ && ~/bin/fix_web_perms.sh';
 
 
--- push_to_production.sh
+++ push_to_production.sh
@@ -4,6 +4,6 @@
 rsync -e 'ssh' -avz --exclude '*.swp' public/ gocoho@gocoho.org:/home/gocoho/public_html/boa/
 
 #reset permissions
-ssh -i ~/.ssh/id_dsa gocoho@gocoho.org 'cd ~/public_html/boa/ && ~/bin/fix_web_perms.sh';
+ssh gocoho@gocoho.org 'cd ~/public_html/boa/ && ~/bin/fix_web_perms.sh';
 
 
